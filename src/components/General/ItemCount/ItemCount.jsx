b873e24116aaee5cf620a1c3866d0c47f0c19a2c
Bautista Roberto <bautiroberto00@gmail.com>
2020-12-22 23:19:55 -0300

    sextocommit

--- src/components/General/ItemCount/ItemCount.jsx
+++
@@ -1,8 +0,0 @@
-
-    function itemCount({item, initial,onAdd})
-
-    useEffect(() =>{
-        setCont(cont + 1);
-    }, [cont])
-
-    <itemCount stock = '5' initial = '1'/>
---
+++ src/components/Home/Index.jsx
@@ -0,0 +1,9 @@
+import Fproduct from './Fproduct/Fproduct'
+
+const Home = () => {
+    return (
+        <Fproduct />
+    )
+}
+
+export default Home;
